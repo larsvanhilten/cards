358ddb6e958b03c9605654dab20562332d6b1362
Lars <larsvh8@gmail.com>
2021-05-30 14:15:59 +0200

    Implement healthcheck

--- cards-backend/src/app.module.ts
+++ cards-backend/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { HealthcheckController } from './controllers/healthcheck.controller';
 import { LobbyGateway } from './gateways/lobby.gateway';
 import { OhHellGateway } from './gateways/oh-hell.gateway';
 import { GameService } from './services/game/game.service';
@@ -6,7 +7,7 @@ import { LobbyService } from './services/lobby/lobby.service';
 
 @Module({
   imports: [],
-  controllers: [],
+  controllers: [HealthcheckController],
   providers: [LobbyGateway, OhHellGateway, LobbyService, GameService],
 })
 export class AppModule {}
